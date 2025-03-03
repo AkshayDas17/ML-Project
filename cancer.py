108e06c71b22c6159fa51db6794e0e452c6c71f1
Akshay <95156861+AkshayDas17@users.noreply.github.com>
2025-03-03 14:11:35 +0530

    Add files via upload

---
+++ cancer.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+Model=pickle.load(open('F:\Learn\oracuz\Streamlit\cancer\cancer','rb'))
+c=st.number_input('Clump Thickness')
+size=st.number_input('Uniformity of Cell Size')
+shape=st.number_input('Uniformity of Cell Shape')
+m=st.number_input('Marginal Adhesion')
+e=st.number_input('Single Epithelial Cell Size')
+b=st.number_input('Bland Chromatin')
+n=st.number_input('Normal Nucleoli')
+mi=st.number_input('Mitoses')
+if st.button('Check cancer'):
+    pred=Model.predict([[c,size,shape,m,e,b,n,mi]])
+    if pred==0:
+        st.success('NO sign of cancer')
+        st.balloons()
+    else:
+        st.warning('High chance of cancer')    
